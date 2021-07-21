1ded3c859b532ac818206afbc047390b20592819
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-21 14:10:35 +0530

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const hello= ()=>console.log('I am first');
+const timer=()=>{setTimeout(()=>console.log("I'm second but I printed as fourth since I am pushed to event loop"),1000)
+new Promise((resolve,reject)=>{
+    resolve('I am resolved before timer')
+}).then( 
+    res=>console.log(res)
+)
+            };
+const hi =()=>console.log("I'm Third")
+const makepromise= ()=>{
+  
+}
+
+hello()
+timer()
+hi()
+
